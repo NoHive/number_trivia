d2b749e5b5bd87acd3adeb03e0e08f5e5cace1a2
NoHive <zar.harustat@googlemail.com>
2019-12-27 11:35:52 +0100

    a

--- lib/features/number_trivia/domain/usecases/get_concrete_number_trivia.dart
+++ lib/features/number_trivia/domain/usecases/get_concrete_number_trivia.dart
@@ -1,6 +1,26 @@
+import 'package:dartz/dartz.dart';
+import 'package:equatable/equatable.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:meta/meta.dart';
+
+import '../../../../core/error/failure.dart';
+import '../../../../core/usecases/usecase.dart';
+import '../entities/number_trivia.dart';
 import '../repositories/number_trivia_repository.dart';
 
-class GetConcreteNumberTrivia {
+class GetConcreteNumberTrivia  implements UseCase<NumberTrivia, Params>{
   final NumberTriviaReporitory reporitory;
   GetConcreteNumberTrivia(this.reporitory);
+  Future<Either<Failure, NumberTrivia>> call({Params params}) async {
+      return await reporitory.getConcreteNumberTrivia(params.number);
+  }
+}
+
+class Params extends Equatable{
+  final int number;
+  Params({@required this.number});
+
+  @override
+  // TODO: implement props
+  List<Object> get props => [number];
 }
--- test/features/number_trivia/domain/usecases/get_concrete_number_trivia_test.dart
+++ test/features/number_trivia/domain/usecases/get_concrete_number_trivia_test.dart
@@ -1,5 +1,8 @@
+import 'package:dartz/dartz.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:mockito/mockito.dart';
+import 'package:number_trivia/core/error/failure.dart';
+import 'package:number_trivia/features/number_trivia/domain/entities/number_trivia.dart';
 import 'package:number_trivia/features/number_trivia/domain/repositories/number_trivia_repository.dart';
 import 'package:number_trivia/features/number_trivia/domain/usecases/get_concrete_number_trivia.dart';
 
@@ -17,4 +20,25 @@ void main(){
       mockNumberTriviaRepository = MockNumberTriviaRepository();
       usecase = GetConcreteNumberTrivia(mockNumberTriviaRepository);
   });
+
+  final int  tNumber = 1;
+  final NumberTrivia tNumberTrivia = NumberTrivia(number: 1, text: 'test');
+
+  test(
+    'should get trivia for the number from number repositiory',
+      () async{
+      // arrange
+      when(mockNumberTriviaRepository.getConcreteNumberTrivia(any)).thenAnswer((_) async => Right(tNumberTrivia));
+      // act
+      final Either<Failure, NumberTrivia> result = await  usecase(params: Params(number: tNumber));
+
+      //assert
+      expect(result, Right(tNumberTrivia));
+      verify(mockNumberTriviaRepository.getConcreteNumberTrivia(tNumber));
+      verifyNoMoreInteractions(mockNumberTriviaRepository);
+
+      }
+  );
+
+
 }
