981a87d65dbb1451c1cc8422dddcd193d7d71d25
NoHive <zar.harustat@googlemail.com>
2019-12-27 09:45:05 +0100

    lesson1

---
+++ lib/features/number_trivia/domain/usecases/get_concrete_number_trivia.dart
@@ -0,0 +1,6 @@
+import '../repositories/number_trivia_repository.dart';
+
+class GetConcreteNumberTrivia {
+  final NumberTriviaReporitory reporitory;
+  GetConcreteNumberTrivia(this.reporitory);
+}
